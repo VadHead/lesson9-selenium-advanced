52c379abac0c3e16bf0f7a7cbb0842b6f99974f1
Viktor Maksymenko <viktor.maksymenko@ab-soft.net>
2022-08-09 23:01:15 +0200

    Init commit

---
+++ src/test/java/net/absoft/BaseTest.java
@@ -0,0 +1,27 @@
+package net.absoft;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+  protected WebDriver driver;
+
+  @BeforeSuite
+  public void setUpChromeDriver() {
+    WebDriverManager.chromedriver().setup();
+  }
+
+  @BeforeMethod
+  public void initChromeDriver() {
+    driver = new ChromeDriver();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void closeDriver() {
+    driver.quit();
+  }
+}
