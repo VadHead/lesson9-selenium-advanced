1164ceb727c6a561f7b56dc49f1d98f1ff599cd7
Viktor Maksymenko <viktor.maksymenko@ab-soft.net>
2022-08-13 21:06:35 +0200

    Optimised imports in IFrameTest.java

--- src/test/java/net/absoft/switchto/IFrameTest.java
+++ src/test/java/net/absoft/switchto/IFrameTest.java
@@ -4,7 +4,6 @@ import static org.testng.Assert.assertEquals;
 
 import net.absoft.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
